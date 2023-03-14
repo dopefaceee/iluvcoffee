3de97e99cb3ab6f9d5a5f5dfbef68ca4327de78b
Ade Suluh N <ade.suluh@gmail.com>
2023-03-14 18:17:39 +0700

    add entitiy

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,9 +1,12 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
 import { Module } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entitiy';
 
 @Module({
     controllers: [CoffeesController],
     providers: [CoffeesService],
+    imports: [TypeOrmModule.forFeature([Coffee]),],
 })
 export class CoffeesModule {}
